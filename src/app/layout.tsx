37f812466bcd2843fb77cb5c92e50e079341de6a
Roshan Sha <roshansha2021@gmail.com>
2026-05-28 21:51:15 +0530

    dd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" type="image/x-icon" href="/assets/favicon/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" />
